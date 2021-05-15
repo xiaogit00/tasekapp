51ee5159943fdcff81b6526ec70c004309b12d26
xiaogit00 <leibingguo@gmail.com>
2021-05-15 10:20:52 +0800

    changed view to point to benny_2

--- resources/views/beneficiaries_2.blade.php
+++ resources/views/beneficiaries_2.blade.php
@@ -36,6 +36,43 @@
                 </div>
             </div>
             <!-- main content to be displayed here -->
+            <!-- Page Content  -->
+          <div id="content" class="p-4 p-md-5 pt-5">
+
+            <h2 class="mb-4">Beneficiary table</h2>
+            <table class='table'>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Age</th>
+                  <th>Programs</th>
+                  <th>Number</th>
+                  <th>Address</th>
+                </tr>
+              </thead>
+              <tbody>
+                <!-- This is a loop for the table rows -->
+                @for ($i =0; $i<100; $i++)
+
+                <tr class='table-hover'>
+                  <td><a href="/{{$person[$i]['id']}}">{{$person[$i]['name']}}</a></td>
+                  <td>{{$person[$i]['dob']}}</td>
+                  <td>
+                  @foreach ($person[$i]->programs as $program)
+                  {{$program->name}}
+                  @endforeach
+                  </td>
+                  <!-- <td>{{$person[$i]['programs']}}</td> -->
+
+                  <td>{{$person[$i]['phoneNum']}}</td>
+                  <td>{{$person[$i]['address']}}</td>
+                </tr>
+                @endfor
+
+              </tbody>
+            </table>
+
+            </div>
         </div>
     </div>
 
