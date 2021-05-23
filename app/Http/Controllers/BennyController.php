5eb9521d7ca3c47811dfd68be80d78cc9461520f
xiaogit00 <leibingguo@gmail.com>
2021-05-23 14:54:38 +0800

    all

--- app/Http/Controllers/BennyController.php
+++ app/Http/Controllers/BennyController.php
@@ -16,8 +16,9 @@ class BennyController extends Controller
     {
       $persons = Person::with('programs')->Paginate(30);
       if($request->ajax()){
-        $view = view('benny_data2', compact('persons'))->render();
-        return response()->json(['html'=>$view]);
+        // $view = view('benny_data2', compact('persons'))->render();
+        // return response()->json(['html'=>$view]);
+        echo 'dsads';
       }
 
         return view('beneficiaries_2',compact('persons'));
